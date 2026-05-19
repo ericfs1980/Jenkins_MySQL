492a167e2b8c56f9ce378ca74bafb19e758605e5
Eric.Ferreira <ericfs1980@gmail.com>
2026-05-19 10:11:31 -0300

    Insert Users

---
+++ flyway/sql/V005__insert_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users 
+( NAME, email, telefone, cpf ) 
+VALUE 
+( 'Eric Santos', 'eric.santos@gmail.com', '21987037989', '11122233344' ),
+( 'Walkiria Santos', 'walquiria.santos@gmail.com', '21984881001', '55566677788' );
