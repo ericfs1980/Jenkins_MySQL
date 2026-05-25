677512c3512050cde27132e025c6757ab4ea9fce
Eric.Ferreira <ericfs1980@gmail.com>
2026-05-25 11:58:13 -0300

    Testando Update para vários registros

---
+++ flyway/sql/V010__update_endereco_id2_id3.sql
@@ -0,0 +1,11 @@
+UPDATE users
+SET endereco = "Travessa João Raimundo, 90 casa 1"
+WHERE id = 1;
+
+UPDATE users
+SET endereco = "Travessa João Raimundo, 90 casa 1"
+WHERE id = 2;
+
+UPDATE users
+SET endereco = "Travessa João Raimundo, 90 casa 1"
+WHERE id = 3;
